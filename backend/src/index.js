b65add638b1ae7daf2b2e742cb9427f927be6179
Júlia Eduarda Miranda de Sousa <39354498+juliamsousa@users.noreply.github.com>
2020-12-21 09:48:02 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const cors = require ('cors');
+// importa as rotas criadas 
 const routes = require ('./routes');
 
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 // informar ao app que o corpo da requisicao utilizara o formato JSON
 app.use(express.json());
 app.use(cors());
+// diz ao app para utilizar as rotas criadas
 app.use(routes); 
 
+// define que o app servira na porta 3333
 app.listen(3333);
